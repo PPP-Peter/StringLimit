90db0871734a1af56f9c5351b3579a62ccbe8f93
Peter <p.petermanik@gmail.com>
2023-03-09 20:39:16 +0100

    repair

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,27 +1,27 @@
 <template>
-  <DefaultField
-    :field="field"
-    :errors="errors"
-    :show-help-text="showHelpText"
-    :full-width-content="fullWidthContent"
-  >
-    <template #field>
-      <input
-        :id="field.attribute"
-        type="text"
-        class="w-full form-control form-input form-input-bordered"
-        :class="errorClasses"
-        :placeholder="field.name"
-        :max="field.maxLength"
-        v-model="value"
-      />
+    <DefaultField
+        :field="field"
+        :errors="errors"
+        :show-help-text="showHelpText"
+        :full-width-content="fullWidthContent"
+    >
+        <template #field>
+            <input
+                :id="field.attribute"
+                type="text"
+                class="w-full form-control form-input form-input-bordered"
+                :class="errorClasses"
+                :placeholder="field.name"
+                :max="field.maxLength"
+                v-model="value"
+            />
 
-        <p class="my-2 text-light" :class="{ warning: (field.maxLength - value.length) < 0,  good: (field.maxLength - value.length) > 0 }">
-            ostáva {{ (field.maxLength - value.length)}}  znakov
-        </p>
+            <p class="my-2 text-light" :class="{ warning: (field.maxLength - value.length) < 0,  good: (field.maxLength - value.length) > 0 }">
+                ostáva {{ (field.maxLength - value.length)}}  znakov
+            </p>
 
-    </template>
-  </DefaultField>
+        </template>
+    </DefaultField>
 </template>
 
 <style scoped>
@@ -37,30 +37,30 @@
 import { FormField, HandlesValidationErrors } from 'laravel-nova'
 
 export default {
-  mixins: [FormField, HandlesValidationErrors],
+    mixins: [FormField, HandlesValidationErrors],
 
-  props: ['resourceName', 'resourceId', 'field'],
+    props: ['resourceName', 'resourceId', 'field'],
 
     data(){
-      return {
-          //maxLenght: 50
-      }
+        return {
+            //maxLenght: 50
+        }
     },
 
-  methods: {
-    /*
-     * Set the initial, internal value for the field.
-     */
-    setInitialValue() {
-      this.value = this.field.value || ''
-    },
+    methods: {
+        /*
+         * Set the initial, internal value for the field.
+         */
+        setInitialValue() {
+            this.value = this.field.value || ''
+        },
 
-    /**
-     * Fill the given FormData object with the field's internal value.
-     */
-    fill(formData) {
-      formData.append(this.field.attribute, this.value || '')
+        /**
+         * Fill the given FormData object with the field's internal value.
+         */
+        fill(formData) {
+            formData.append(this.field.attribute, this.value || '')
+        },
     },
-  },
 }
 </script>
